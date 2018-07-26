d17c6639aab00cfa97d0ecd5cb6cdf4e99a97c7c
neo <xchxie@sohu.com>
2018-07-26 23:00:10 +0800

    dubbo-admin.war

--- dubboconsumer/src/main/java/com/neo/test/Consumer.java
+++ dubboconsumer/src/main/java/com/neo/test/Consumer.java
@@ -7,8 +7,15 @@ public class Consumer {
         System.setProperty("java.net.preferIPv4Stack", "true");
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"META-INF/dubbo-consumer.xml"});
         context.start();
-        DubboTestService demoService = (DubboTestService)context.getBean("dubboTestService");
-        String result = demoService.sayHello("Neo");
-        System.out.println(result);
+        while (true) {
+            DubboTestService demoService = (DubboTestService)context.getBean("dubboTestService");
+            String result = demoService.sayHello("Neo");
+            System.out.println(result);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
